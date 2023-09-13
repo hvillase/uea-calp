6af803684fb75dc7ce50cbcc4849d0de39a76b0d
hvillase <hernani.vr@gmail.com>
2023-09-13 10:06:19 -0600

    clase6

---
+++ clase-6/cav6-array.js
@@ -0,0 +1,6 @@
+//Arrays
+//voronoi(2).color(1,0,[1,2,3].fast(2)).out()
+
+////shape(2,0.1).rotate([4,-4].ease('linear')).out()
+//shape(3, 0.1).scrollX([-0.2,0.2]).add(shape(4, 0.1).scrollY(0.2).scrollX([-0.2,0.2].offset(0.5))).out()
+//shape(4).scrollX([1,2,3,4].fit(-0.01, 0.01)).out()
---
+++ clase-6/cav6-init.js
@@ -0,0 +1,11 @@
+//s0.initCam()
+//src(s0).out()
+
+//s0.initImage("/home/hernani/uea-calp/clase-6/media/foto1.jpg")
+//src(s0).color(3,[2, 0.5, 0.1].fast(2),0.2).modulate(osc(),0.01).out()
+
+//s0.initVideo("/home/hernani/uea-calp/clase-6/media/video1.mp4")
+//src(s0).out()
+
+//s0.initScreen()
+//src(s0).out()
