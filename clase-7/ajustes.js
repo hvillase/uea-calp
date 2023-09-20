e545ef22ccb65c55e6e9a55bd4a64c4392193078
hvillase <hernani.vr@gmail.com>
2023-09-19 22:13:31 -0600

    clase7

---
+++ clase-7/ajustes.js
@@ -0,0 +1,14 @@
+//osc().out(o0)
+//noise().color(1,0,1).out(o1)
+//voronoi().out(o2)
+//osc(2,2,2).out(o3)
+//render()
+
+//setResolution(50,50)
+//osc().out()
+//hush()
+
+//speed = 3
+//bpm = 60
+//width
+//height
